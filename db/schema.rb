fd24a2081d152f89445f05a73f121440501e6efb
Greg Lazarev <greg@thoughtbot.com>
2014-12-11 20:40:42 -0800

    Introduce StyleGuideConfig
    * Add `StyleGuideConfig` model and table
    * Use hstore for `rules` column to hold the hash of rules
    * Define default Ruby style guide config

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150108003045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "builds", force: :cascade do |t|
     t.text     "violations_archive"
@@ -62,6 +63,17 @@ ActiveRecord::Schema.define(version: 20150108003045) do
   add_index "repos", ["active"], name: "index_repos_on_active", using: :btree
   add_index "repos", ["full_github_name"], name: "index_repos_on_full_github_name", unique: true, using: :btree
   add_index "repos", ["github_id"], name: "index_repos_on_github_id", using: :btree
+
+  create_table "style_guide_configs", force: true do |t|
+    t.string   "name",       null: false
+    t.boolean  "enabled",    null: false
+    t.integer  "owner_id",   null: false
+    t.hstore   "rules"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "style_guide_configs", ["owner_id", "name"], name: "index_style_guide_configs_on_owner_id_and_name", unique: true, using: :btree
 
   create_table "subscriptions", force: :cascade do |t|
     t.datetime "created_at",                                                               null: false
